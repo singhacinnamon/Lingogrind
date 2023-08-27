188be6e824b41ac21831caac2ba84012e54f3819
Campbell <csinghas@ucsd.edu>
2023-08-27 09:13:09 -0700

    Lesson POST request handler

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,8 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = ('id', 'lang', 'prio', 'name', 'file')
+
+class CreateLessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ('lang', 'prio', 'name', 'file')
--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics
-from .serializers import LessonSerializer
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import LessonSerializer, CreateLessonSerializer
 from .models import Lesson
 
 
@@ -12,3 +14,16 @@ def home(request):
 class LessonView(generics.CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+class CreateLessonView(APIView):
+    serializer_class = CreateLessonSerializer
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            lang = serializer.data.get('lang')
+            prio = serializer.data.get('prio')
+            name = serializer.data.get('name')
+            file = serializer.data.get('file')
+            lesson = Lesson(lang=lang, prio=prio, name=name, file=file)
+            lesson.save()
+            return Response(LessonSerializer(lesson).data, status=status.HTTP_201_CREATED)
